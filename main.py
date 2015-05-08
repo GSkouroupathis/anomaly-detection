d90f6d6f437349eb00f33875d29acf8885e8520c
George <george@FUCKUP.(none)>
2015-05-08 06:57:04 +0100

    Better Filter & DB

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import dbOp, datetime, testing, attack
 from ekfdetector import *
 from cusumdetector import *
 from terattack import *
+from mcattack import *
 from plotter import *
 import numpy
 
@@ -17,7 +18,7 @@ def init():
 
 	# Filter readings
 	allFilteredReadings = []
-	readingsFilter = ReadingsFilter(threshold=25)
+	readingsFilter = ReadingsFilter(threshold=4)
 	inp = 0
 	for node in parsedReadings.keys():
 
@@ -38,29 +39,38 @@ def init():
 	dbOp.insertAllReadings(allFilteredReadings)
 
 	# Calculate and insert Covariances
-	nodes = dbOp.selectAllNodes()
+	'''
+	nodes = map(lambda x: x[0], dbOp.selectAllNodes())
 	for (i, node1) in enumerate(nodes):
+		i += 1
 		readings1 = dbOp.selectReadingsFromNode(node1)
 		for node2 in nodes[i+1:]:
 			readings2 = dbOp.selectReadingsFromNode(node2)
 			sL = min(len(readings1), len(readings2))
-			cov = numpy.cov(readings1[:sL], readings[:sL])[0][1]
+			cov = numpy.cov(readings1[:sL], readings2[:sL])[0][1]
 			dbOp.insertCov(node1, node2, cov)
+	'''
 	dbOp.closeConnectionToDatabase()
-
+	
 #init()
-'''
+
 readings = []
 dbOp.connectToDatabase("data/db")
 r3 = map(lambda x: x[0], dbOp.selectReadingsFromNode(3))
-r10 = map(lambda x: x[0], dbOp.selectReadingsFromNode(6))
-r26 = map(lambda x: x[0], dbOp.selectReadingsFromNode(26))
-r11 = map(lambda x: x[0], dbOp.selectReadingsFromNode(11))
-r42 = map(lambda x: x[0], dbOp.selectReadingsFromNode(42))
+r4 = map(lambda x: x[0], dbOp.selectReadingsFromNode(4))
+r6 = map(lambda x: x[0], dbOp.selectReadingsFromNode(6))
+r8 = map(lambda x: x[0], dbOp.selectReadingsFromNode(8))
+r13 = map(lambda x: x[0], dbOp.selectReadingsFromNode(13))
+r18 = map(lambda x: x[0], dbOp.selectReadingsFromNode(18))
+r23 = map(lambda x: x[0], dbOp.selectReadingsFromNode(23))
+r25 = map(lambda x: x[0], dbOp.selectReadingsFromNode(25))
+r49 = map(lambda x: x[0], dbOp.selectReadingsFromNode(49))
+'''
 for i in map(lambda x: x[0], dbOp.selectAllNodes()):
 	if i != 3: readings.append( (i, map(lambda x: x[0], dbOp.selectReadingsFromNode(i))) )
-dbOp.closeConnectionToDatabase()
 '''
+dbOp.closeConnectionToDatabase()
+
 
 
 
@@ -93,11 +103,21 @@ falseSignal = terMimicry.attack(r3, 28, 0, [r3])[0]
 '''
 
 
+
+# MC mimicry
+'''
+mcMimicry = MCMimicry(r3)
+'''
+
+
+
 # Plot stuff
 import matplotlib.pyplot as plt
-plt.axis('equal')
-plt.plot([1.2,1], 'b')
+#plt.axis('equal')
+
+plt.plot(r6, 'b')
 plt.show()
-
-
-
+plt.plot(r13, 'b')
+plt.show()
+plt.plot(r18, 'b')
+plt.show()
--- mcattack.py
+++ mcattack.py
@@ -1,28 +1,31 @@
 from attack import *
-import dbOp
+import dbOp, math, numpy
 from scipy.cluster.vq import kmeans2
-import numpy
 
 class MCMimicry(Attack):
 	# Constructor
 	def __init__(self, dataset):
 		self.dataset = dataset
-		self.w = self.choose_window_size(dataset)
-
+		yoyo = self.choose_window_size(dataset)
+		self.w = yoyo[0]
+		print yoyo
+				
 	# 1 ##########################################################################
 	# Chooses the appropriate window size w
 	def choose_window_size(self, dataset=None):
-		if !dataset: dataset = self.dataset
+		if not dataset: dataset = self.dataset
 		
 		bestScore = -1
 		
-		for w in [5, 10, 15, 100]:
-			segment_list = numpy.array(segment_signal(dataset, w, 0))
-			(centroids, labels) = cluster(segment_list)
+		for w in [5, 7, 8, 9, 10, 12, 15, 20, 30]:
+			print w
+			segment_list = numpy.array(self.segment_signal(dataset, w, 0))
+			(centroids, labels) = self.cluster(segment_list)
 			# the sum of segments in each cluster
-			clusterCount = [labels.count(i) for i in range(len(centroids))]
-			condProbTable = create_cond_prob_table(centroids, labels, clusterCount)
-			tableScore = eval_cond_prob_table(cond_prob_table, clusterCount)
+			clusterCount = numpy.bincount(labels)
+			condProbTable = self.create_cond_prob_table(centroids, labels, clusterCount)
+			tableScore = self.eval_cond_prob_table(condProbTable, clusterCount)
+			print tableScore
 			if tableScore > bestScore:
 				bestW = w
 				bestSegments = segment_list
@@ -30,30 +33,31 @@ class MCMimicry(Attack):
 				bestCondProbTable = condProbTable
 				bestScore = tableScore
 				
-		return (bestW, bestSegments, bestCentroids, bestLabels, bestCondProbTable)
-			
+		return (bestW, bestSegments, bestCentroids, bestLabels, bestCondProbTable, bestScore)
+
 	# 2 ##########################################################################
 	# Divides signal into segments
 	# a: signal
 	# w: window size
 	# h: hop size
-	def segment_signal(z, w, h):
+	def segment_signal(self, z, w, h):
 		segment_list = []
 		for (i, r) in enumerate(z[::w]):
 			segment = z[i*w:i*w+w]
-			segment_list.append(segment)
+			if len(segment) == w:
+				segment_list.append(segment)
 		return segment_list
 
 	# Clusters the segments into K clusters
-	def cluster(segments):
+	def cluster(self, segments):
 		prevAvDist = 99999999
 		prevCentroids = numpy.array([])
 		prevLabels = []
 		# tries to find K
 		for K in range(2,100):
-			(centroids, labels) = k_means(segments, K)
-			avDist = get_average_distance(centroids, labels, segments)
-			isKnee = find_knee(prevAvDist, avDist)
+			(centroids, labels) = self.k_means(segments, K)
+			avDist = self.get_average_distance(centroids, labels, segments)
+			isKnee = self.find_knee(prevAvDist, avDist)
 			# if correct K found
 			if isKnee:
 				return (prevCentroids, prevLabels)
@@ -64,34 +68,35 @@ class MCMimicry(Attack):
 		return (None, None)
 			
 	# Creates conditional probabilities table
-	def create_cond_prob_table(centroids, labels, clusterCount):
+	def create_cond_prob_table(self, centroids, labels, clusterCount):
 		# the sum of segments in cluster given previous cluster
 		countTable = [[0]*len(centroids) for i in range(len(centroids))]
 		for (i, thisCluster) in enumerate(labels[1:]):
 			lastCluster = labels[i]
 			countTable[lastCluster][thisCluster] += 1
-		
-		condProbTable = dirichlet(countTable, clusterCount, K=len(centroids))
-		return (condProbTable, clusterCount)
+		condProbTable = self.dirichlet(countTable, clusterCount, K=len(centroids))
+		return condProbTable
 			
 	# Evaluates conditional probabilities table
-	def eval_cond_prob_table(condProbTable, clusterCount):
+	def eval_cond_prob_table(self, condProbTable, clusterCount):
 		negentropies = []
 		for row in condProbTable:
-			negentropies.append(negentropy(row))
-		score = w_m_negentropy(negentropies, clusterCount):
+
+
+			negentropies.append(self.negentropy(row))
+		score = self.w_m_negentropy(negentropies, clusterCount)
 		return score
 		
 	# 3 ##########################################################################
 	# Performs K-means
 	# K: number of clusters
-	def k_means(segments, K):
+	def k_means(self, segments, K):
 		(centroids, labels) = kmeans2(segments, K)
 		return (centroids, labels)
 		
 	# Gets average distance of segments to
 	# their corresponding cluster centers
-	def get_average_distance(centroids, labels, segments):
+	def get_average_distance(self, centroids, labels, segments):
 		totalDist = 0
 		for (i, segment) in enumerate(segments):
 			centroid = centroids[labels[i]]
@@ -100,28 +105,29 @@ class MCMimicry(Attack):
 		return totalDist / len(segments)
 		
 	# Finds knee on graph by computing the derivative
-	def find_knee(prevAvDist, avDist):
-		return prevAvDist - avDist > -0.3
+	def find_knee(self, prevAvDist, avDist):
+		return avDist - prevAvDist > -0.3
 	
 	# 3 ##########################################################################
 	# Applies Dirichlet distribution to create Conditional Probabilities Table
-	def dirichlet(countTable, clusterCount, K):
+	def dirichlet(self, countTable, clusterCount, K):
 		for (prevCluster, row) in enumerate(countTable):
 			for (thisCluster, thisClusterCount) in enumerate(row):
-				row[thisCluster] = (thisClusterCount + 1) / (clusterCount[prevCluster] + K)
+				row[thisCluster] = (thisClusterCount + 1)*1.0 / (clusterCount[prevCluster] + K)
 		return countTable
 
 	# 3 ##########################################################################	# Computes entropy of distribution
-	def entropy(distribution):
+	def entropy(self, distribution):
+
 		return -sum([p*math.log(p,10) for p in distribution])
 		
 	# Computes negentropy of distribution against uniform distribution
-	def negentropy(distribution):
+	def negentropy(self, distribution):
 		uniform = [1.0/len(distribution) for i in distribution]
-		return entropy(uniform) - entropy(distribution)
+		return self.entropy(uniform) - self.entropy(distribution)
 	
 	# Weighted mean negentropy
-	def w_m_negentropy(negentropies, clusterCount):
+	def w_m_negentropy(self, negentropies, clusterCount):
 		totalClusterCount = sum(clusterCount)
 		return sum(map(lambda x: x[0]*(x[1]*1.0/totalClusterCount), zip(negentropies, clusterCount)))
 
